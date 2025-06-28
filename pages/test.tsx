58f5531111fae0a69e1dbf3c6d27ef9c097cab30
CanvernSH <canvernsh@gmail.com>
2025-06-28 23:58:34 +0100

    Update test.tsx Cleaning

--- pages/test.tsx
+++ pages/test.tsx
@@ -5,13 +5,11 @@ const TestPage = () => {
 
   return (
     <div>
-      // Time
       <div>
         <h1> Time</h1>
         <p> Time of page load: {now}</p>
       </div>
   
-      // Title
       <div>
         <h1>This is the Test Page</h1>
         <p>Welcome to your new blank page!</p>
