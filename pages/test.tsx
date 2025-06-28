37115eb4ae36d31cd82110cb938e73c8437fba65
Canvern <146818709+Canvern@users.noreply.github.com>
2025-06-28 20:34:17 +0100

    Initial commit of Higher or Lower game

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>This is the Test Page</h1>
+      <p>Welcome to your new blank page!</p>
+    </div>
+  );
+};
+
+export default TestPage;
